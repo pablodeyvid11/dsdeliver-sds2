ed4294133c0408aee42f36b42ef0a7993b42c463
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-01-08 15:21:04 -0300

    Project Created

---
+++ backend/src/main/java/com/pablodeyvid/dsdeliverpdp/DsdeliverpdpApplication.java
@@ -0,0 +1,13 @@
+package com.pablodeyvid.dsdeliverpdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverpdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverpdpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pablodeyvid/dsdeliverpdp/DsdeliverpdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablodeyvid.dsdeliverpdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverpdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9406cd4ea2d052484a70a0b13f0857a28e5f163d
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-01-08 17:08:59 -0300

    Seed

--- backend/src/main/java/com/pablodeyvid/dsdeliverpdp/entities/Product.java
+++ backend/src/main/java/com/pablodeyvid/dsdeliverpdp/entities/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "td_product")
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	

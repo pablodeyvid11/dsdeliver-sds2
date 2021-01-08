6524ec5599ce3bcfad32d6113589f44cf9f20ebe
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-01-08 19:32:10 -0300

    POST/orders

--- backend/src/main/java/com/pablodeyvid/dsdeliverpdp/services/OrderService.java
+++ backend/src/main/java/com/pablodeyvid/dsdeliverpdp/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.pablodeyvid.dsdeliverpdp.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,13 +9,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pablodeyvid.dsdeliverpdp.dto.OrderDTO;
+import com.pablodeyvid.dsdeliverpdp.dto.ProductDTO;
 import com.pablodeyvid.dsdeliverpdp.entities.Order;
+import com.pablodeyvid.dsdeliverpdp.entities.Product;
+import com.pablodeyvid.dsdeliverpdp.entities.enums.OrderStatus;
 import com.pablodeyvid.dsdeliverpdp.repositories.OrderRepository;
+import com.pablodeyvid.dsdeliverpdp.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true) /*
 									 * Garantir transação e fechamento de conexão com o banco; ReadOnly = Diz que é
@@ -24,5 +32,19 @@ public class OrderService {
 		List<Order> list = repository.findOrderWithProducts();
 
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLatitude(), Instant.now(),
+				OrderStatus.PENDING);
+		
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }

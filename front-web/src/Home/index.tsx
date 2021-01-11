4d0f0f968c7a581d46a1ee5379f3d927a02c789d
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-01-11 15:33:37 -0300

    Add footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,33 +1,37 @@
 import './styles.css';
-import {ReactComponent as MainImage} from './main.svg';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido
-                        <br/>
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido
+                        <br />
                         que entregamos
-                        <br/>
+                        <br />
                         para você!
                     </h1>
 
-                    <h3 className="home-subtitle">
-                        Escolha seu pedido e em poucos minutos
-                        <br/>
+                        <h3 className="home-subtitle">
+                            Escolha seu pedido e em poucos minutos
+                        <br />
                         levaremos na sua porta
                     </h3>
-                    <a className="home-btn-order" href="orders">
-                        FAZER PEDIDO
+                        <a className="home-btn-order" href="orders">
+                            FAZER PEDIDO
                     </a>
-                </div>
-                <div className="home-image">
-                    <MainImage/>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer/>
+        </>
     );
 }
 

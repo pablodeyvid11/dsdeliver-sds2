07a4a9b69db4bce44665a24adc5d26053e1815f8
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-01-14 23:39:38 -0300

    fix github icon footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import {ReactComponent as Youtube} from './Youtube.svg';
+import {ReactComponent as GitHub} from './GitHub.svg';
 import {ReactComponent as Linkedin} from './Linkedin.svg';
 import {ReactComponent as Instagram} from './Instagram.svg';
 
@@ -8,13 +8,13 @@ function Footer() {
         <footer className="main-footer">
             Acesse as nossas redes sociais e saiba mais sobre a gente
             <div className="footer-icons">
-                <a href="https://www.youtube.com/devsuperior" target="_new">
-                    <Youtube/>
+                <a href="https://github.com/pablodeyvid11" target="_new">
+                    <GitHub/>
                 </a>
-                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                <a href="https://www.linkedin.com/in/pablo-deyvid-de-paiva-7a59261a1/" target="_new">
                     <Linkedin/>
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <a href="https://www.instagram.com/pablo_deyvid/" target="_new">
                     <Instagram/>
                 </a>
             </div>
